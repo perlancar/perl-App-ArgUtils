d81c74821e9ff2356209c975876d57a52c79981e
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-08-26 11:37:48 +0700

    Initial commit

---
+++ lib/App/ArgUtils.pm
@@ -0,0 +1,18 @@
+package App::ArgUtils;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: CLI utilities related to command-line arguments
+
+=head1 DESCRIPTION
+
+This distribution contains CLI utilities related to command-line arguments:
+
+# INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
